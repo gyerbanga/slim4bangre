85aeec37f5188d95e4f9027209d610b9a637347d
Gaston <gyerbanga@innosys.fr>
2020-02-12 12:27:22 +0100

    routes : ajout de nouvelles routes

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,9 @@
 <?php
 
-use Slim\Http\Response;
-use Slim\Http\ServerRequest;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', function (ServerRequest $request, Response $response) {
-        $response->getBody()->write('Hello, World!');
-
-        return $response;
-    });
+    $app->get('/', \App\Action\HomeAction::class);
+    $app->get('/users/{id}', \App\Action\UserReadAction::class);
+    $app->post('/users', \App\Action\UserCreateAction::class);
 };
