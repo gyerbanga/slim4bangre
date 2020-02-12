6fa1aca485f9d237980266e07993c0abd9dc981c
Gaston <gyerbanga@innosys.fr>
2020-02-12 12:28:56 +0100

    settings : ajout de paramètres

--- config/settings.php
+++ config/settings.php
@@ -30,4 +30,28 @@ $settings['error_handler_middleware'] = [
     'log_error_details' => true,
 ];
 
+
+// Database settings
+$settings['db'] = [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'username' => 'innodev',
+    'database' => 'caribis_gaston_planning',
+    'password' => 'Kr47052016',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'flags' => [
+        // Turn off persistent connections
+        PDO::ATTR_PERSISTENT => false,
+        // Enable exceptions
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Emulate prepared statements
+        PDO::ATTR_EMULATE_PREPARES => true,
+        // Set default fetch mode to array
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // Set character set
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+    ],
+];
+
 return $settings;
