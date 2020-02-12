ac3c9c7e4ca1952aa4df94d49261132f17cab79b
Gaston <gyerbanga@innosys.fr>
2020-02-12 12:30:36 +0100

    container : modif

--- config/container.php
+++ config/container.php
@@ -14,11 +14,24 @@ return [
         AppFactory::setContainer($container);
         $app = AppFactory::create();
 
-        // Optional: Set the base path to run the app in a sub-directory
-        // The public directory must not be part of the base path
-        //$app->setBasePath('/slim4-tutorial');
+        // Optional: Set the base path to run the app in a subdirectory.
+        $app->setBasePath('/slim4bangre');
 
         return $app;
+    },
+
+    PDO::class => function (ContainerInterface $container) {
+        $config = $container->get(Configuration::class);
+
+        $host = $config->getString('db.host');
+        $dbname =  $config->getString('db.database');
+        $username = $config->getString('db.username');
+        $password = $config->getString('db.password');
+        $charset = $config->getString('db.charset');
+        $flags = $config->getArray('db.flags');
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+        return new PDO($dsn, $username, $password, $flags);
     },
 
 ];
