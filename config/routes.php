23c32ab59ae94e5e8de7d2f7bf21d0010308f274
Gaston <gyerbanga@innosys.fr>
2020-02-12 16:49:21 +0100

    routes : ajout de nouvelles fonctionnalités

--- config/routes.php
+++ config/routes.php
@@ -6,4 +6,6 @@ return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class);
     $app->get('/users/{id}', \App\Action\UserReadAction::class);
     $app->post('/users', \App\Action\UserCreateAction::class);
+    $app->get('/hello/{name}', \App\Action\Hello\HelloAction::class)->setName('hello');
+    $app->get('/site', \App\Action\Home\HomeAction::class)->setName('root');
 };
