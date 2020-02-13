665ebe7ce064bb52cdf23345a5f7cbf1043d5e66
Gaston <gyerbanga@innosys.fr>
2020-02-13 15:39:38 +0100

    routes : nouvelle fonctionnalité

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@ use Slim\App;
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class);
     $app->get('/users/{id}', \App\Action\UserReadAction::class);
-    $app->post('/users', \App\Action\UserCreateAction::class);
+    //$app->post('/users', \App\Action\UserCreateAction::class);
     $app->get('/hello/{name}', \App\Action\Hello\HelloAction::class)->setName('hello');
     $app->get('/site', \App\Action\Home\HomeAction::class)->setName('root');
 };
