6e75b03390ad8ccea467a231a4575aac47a0e51e
Gaston <gyerbanga@innosys.fr>
2020-02-14 12:13:49 +0100

    routes : nouvelles routes

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,17 @@
 <?php
 
+// Define app routes
+
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', \App\Action\HomeAction::class);
-    $app->get('/users/{id}', \App\Action\UserReadAction::class);
-    //$app->post('/users', \App\Action\UserCreateAction::class);
+    $app->get('/', \App\Action\Home\HomeAction::class)->setName('root');
+
     $app->get('/hello/{name}', \App\Action\Hello\HelloAction::class)->setName('hello');
-    $app->get('/site', \App\Action\Home\HomeAction::class)->setName('root');
+
+    $app->get('/users', \App\Action\User\UserListAction::class)->setName('user-list');
+
+    $app->post('/users/datatable', \App\Action\User\UserListDataTableAction::class)->setName('user-datatable');
+
+    $app->post('/api/users', \App\Action\User\UserCreateAction::class)->setName('api-user-create');
 };
