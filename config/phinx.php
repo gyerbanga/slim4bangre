a6ffb282e1a2a1680c4edc6e98b98274a5a4c639
Gaston <gyerbanga@innosys.fr>
2020-02-14 12:12:57 +0100

    settings : route cache

---
+++ config/phinx.php
@@ -0,0 +1,22 @@
+<?php
+/*   Add configuration component */
+
+use Selective\Config\Configuration;
+use Slim\App;
+
+/** @var App $app */
+$app = require __DIR__ . '/../config/bootstrap.php';
+
+$container = $app->getContainer();
+$pdo = $container->get(PDO::class);
+$config = $container->get(Configuration::class);
+$database = $config->getString('db.database');
+$phinx = $config->getArray('phinx');
+
+$phinx['environments']['local'] = [
+    // Set database name
+    'name' => $database,
+    'connection' => $pdo,
+];
+
+return $phinx;
---
+++ config/staging.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/*     Add route cache     */
+
+// Staging environment
+$settings['env'] = 'staging';
+
+$settings['router']['cache_file'] = $settings['temp'] . '/route-cache.php';
+
+// Database
+$settings['db']['database'] = 'staging_dbname';
