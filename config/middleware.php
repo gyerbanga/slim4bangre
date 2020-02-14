46cc00c5aff07ca48a186f9bd9fdc79ef411ce5d
Gaston <gyerbanga@innosys.fr>
2020-02-14 12:14:08 +0100

    middleware: Register error renderers for specific content-types

--- config/middleware.php
+++ config/middleware.php
@@ -1,16 +1,29 @@
 <?php
 
+use App\Handler\HtmlErrorRenderer;
+use App\Handler\JsonErrorRenderer;
+use App\Middleware\LocaleSessionMiddleware;
+use App\Middleware\SessionMiddleware;
+use App\Middleware\TranslatorMiddleware;
+use Selective\BasePath\BasePathMiddleware;
 use Selective\Config\Configuration;
+use Selective\Validation\Middleware\ValidationExceptionMiddleware;
 use Slim\App;
+use Slim\Views\TwigMiddleware;
 
 return function (App $app) {
+    $container = $app->getContainer();
+
     // Parse json, form data and xml
     $app->addBodyParsingMiddleware();
 
-    // Add global middleware to app
+    $app->add(ValidationExceptionMiddleware::class);
+    $app->add(TwigMiddleware::class);
+    $app->add(TranslatorMiddleware::class);
+    $app->add(LocaleSessionMiddleware::class);
+    $app->add(SessionMiddleware::class);
     $app->addRoutingMiddleware();
-
-    $container = $app->getContainer();
+    $app->add(BasePathMiddleware::class);
 
     // Error handler
     $settings = $container->get(Configuration::class)->getArray('error_handler_middleware');
@@ -18,5 +31,9 @@ return function (App $app) {
     $logErrors = (bool)$settings['log_errors'];
     $logErrorDetails = (bool)$settings['log_error_details'];
 
-    $app->addErrorMiddleware($displayErrorDetails, $logErrors, $logErrorDetails);
+    $errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, $logErrors, $logErrorDetails);
+
+    $errorHandler = $errorMiddleware->getDefaultErrorHandler();
+    $errorHandler->registerErrorRenderer('text/html', HtmlErrorRenderer::class);
+    $errorHandler->registerErrorRenderer('application/json', JsonErrorRenderer::class);
 };
