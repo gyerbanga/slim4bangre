43db9d9adc8b771854af4a6593053225de2eceb1
Gaston <gyerbanga@innosys.fr>
2020-02-25 16:32:56 +0100

    repertoires: nouveaux repertoires

---
+++ src/classes/ToDO.php
@@ -0,0 +1,157 @@
+<?php
+
+
+class ToDo
+{
+
+    private $id;
+    private $task;
+    private $details;
+    private $date;
+    private $time;
+    private $author;
+    private $status;
+
+    /**
+     * ToDo constructor.
+     */
+    public function __construct($task="", $details="",$author="")
+    {
+        $this->task = $task;
+        $this->details = $details;
+        $this->author = $author;
+        $this->date = date("Y-m-d");
+        $this->time = date("h:i:s");
+    }
+
+    public function create($row)
+    {
+        $this->task = $row["name"];
+        $this->details = $row["details"];
+        $this->author = $row["author"];
+        $this->id = $row["task_id"];
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    /**
+     * @param mixed $task
+     */
+    public function setTask($task)
+    {
+        $this->task = $task;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    /**
+     * @param mixed $details
+     */
+    public function setDetails($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param mixed $time
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+
+
+
+    public function getName()
+    {
+        return "My first Tod";
+    }
+
+}
