349dbfe995e50602f99180a81fa1d9f4bfc24e8a
Gaston <gyerbanga@innosys.fr>
2020-02-28 16:07:56 +0100

    routes : chemin pour $_POST

--- config/routes.php
+++ config/routes.php
@@ -8,4 +8,14 @@ return function (App $app) {
     $app->post('/users', \App\Action\UserCreateAction::class);
     $app->get('/hello/{name}', \App\Action\Hello\HelloAction::class)->setName('hello');
     $app->get('/site', \App\Action\Home\HomeAction::class)->setName('root');
+    $app->put('/books/:id', function ($id) {
+/*    	<form action="/books/1" method="post">
+    ... other form fields here...
+    <input type="hidden" name="_METHOD" value="PUT"/>
+    <input type="submit" value="Update Book"/>
+</form>*/
+
+});
 };
+
+
