2c9969ca1b6e2ed274d3a1119174bb28ba755ce6
Gaston <gyerbanga@innosys.fr>
2020-02-28 16:06:45 +0100

    librairies : nouvelles donnes

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,14 +1,11 @@
 <?php
 
+/*     Replace league/container with php-di/php-di      */
+
 use DI\ContainerBuilder;
 use Slim\App;
 
-
-$test=require_once __DIR__ . '/../vendor/autoload.php';
-
-
-$test=require_once __DIR__ . '/../vendor/autoload.php';
-
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $containerBuilder = new ContainerBuilder();
 
@@ -26,5 +23,6 @@ $app = $container->get(App::class);
 
 // Register middleware
 (require __DIR__ . '/middleware.php')($app);
+
 
 return $app;
